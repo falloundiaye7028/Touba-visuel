7457e3acfc92c332a7a6b0bdcb7705b067ffcb2b
iMac Pro <imacpro@iMac-Pro-de-iMac.local>
2026-05-14 16:46:42 +0000

    feat: ajout TikTok @yoonu_murid_digital et YouTube dans le footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,8 +31,15 @@ export default function Footer() {
             <a href="https://web.facebook.com/agencetoubavisuel/" target="_blank" rel="noopener noreferrer" className="w-9 h-9 bg-vert-800 hover:bg-or-500 rounded-lg flex items-center justify-center transition-colors duration-200">
               <Instagram size={16} className="text-white" />
             </a>
-            <a href="https://web.facebook.com/agencetoubavisuel/" target="_blank" rel="noopener noreferrer" className="w-9 h-9 bg-vert-800 hover:bg-or-500 rounded-lg flex items-center justify-center transition-colors duration-200">
-              <Twitter size={16} className="text-white" />
+            <a href="https://www.youtube.com/@toubainfostv183" target="_blank" rel="noopener noreferrer" className="w-9 h-9 bg-vert-800 hover:bg-red-600 rounded-lg flex items-center justify-center transition-colors duration-200">
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" className="text-white">
+                <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+              </svg>
+            </a>
+            <a href="https://www.tiktok.com/@yoonu_murid_digital" target="_blank" rel="noopener noreferrer" className="w-9 h-9 bg-vert-800 hover:bg-gray-900 rounded-lg flex items-center justify-center transition-colors duration-200">
+              <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor" className="text-white">
+                <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.18 8.18 0 0 0 4.78 1.52V6.75a4.85 4.85 0 0 1-1.01-.06z"/>
+              </svg>
             </a>
           </div>
         </div>
